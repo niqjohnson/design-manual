b74c1e9908b926ebc6243a139bcbf75b5b2e2302
James Wilson <jimmynotjim@me.com>
2017-03-13 17:53:50 -0500

    Initial experiment auto-generating a less color palette

---
+++ scripts/npm/colors/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var path = require( 'path' );
+var fs = require( 'fs' );
+var exec = require( 'child-process-promise' ).exec;
+var parseCSV = require( 'csv' ).parse;
+var transformCSV = require( 'csv' ).transform;
+var root = path.join( __dirname, '..', '..', '..' );
+var colorsSource = path.join( root, '_data', 'cfpb-brand-colors.csv' );
+var colorsLess = path.join( root, '_data', 'colors.less' );
+var family;
+var block;
+
+var transformer = transformCSV( function( record, callback ) {
+    setTimeout( function() {
+      if ( record[0] === 'family' ) {
+        return;
+      }
+
+      if ( family !== record[0] ) {
+        family = record[0];
+        block = '\n\n// ' + family[0].toUpperCase() + family.substring(1) + ' family\n\n';
+      } else {
+        block = '';
+      }
+
+      callback( null, block + '@' + record[1] + ': ' + record[3] + ';\n' );
+    }, 500 );
+  }, { parallel: 10 } );
+
+fs.createReadStream( colorsSource )
+  .pipe( parseCSV() )
+  .pipe( transformer )
+  .pipe( fs.createWriteStream( colorsLess ) );
+
